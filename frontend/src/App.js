3ddf3fd70ca9a0b801cc1b8af98f22f515ac70f0
MoeinDeveloper92 <moeinsamani1992@gmail.com>
2023-08-27 17:54:32 +0200

    some changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,10 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import { Routes, Route } from "react-router-dom"
+import PrivateRoute from './components/PrivateRoute'
 import Header from './components/Header'
 import { ToastContainer } from "react-toastify"
+import NewTicket from './pages/NewTicket'
 import "react-toastify/dist/ReactToastify.css"
 function App() {
   return (
@@ -14,6 +16,9 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
+        <Route path='' element={<PrivateRoute />}>
+          <Route path='/new-ticket' element={<NewTicket />} />
+        </Route>
       </Routes>
       <ToastContainer />
     </div>
